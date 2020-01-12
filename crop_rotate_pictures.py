5de1e0e1b3e7741797bb58c54b7b53ff158cdce4
Kseniia I Isavnina <48221601+iSenya@users.noreply.github.com>
2020-01-13 00:23:18 +0200

    Update crop_rotate_pictures.py

--- crop_rotate_pictures.py
+++ crop_rotate_pictures.py
@@ -1,6 +1,7 @@
 import os, random, string
 from PIL import Image
 from PIL import ImageOps
+#this is a path to my home directory with unprepared pictures from satellites, you can change it to your own path
 folder = r"/home/kseniia/Pictures"
 courts=os.listdir(folder)
 for i in courts:
@@ -9,4 +10,6 @@ for i in courts:
         border = (1240, 400, 1240, 400)
         cr_pic = ImageOps.crop(pic.rotate(n), border)
         cr_resize_pic = cr_pic.resize((224,224), Image.ANTIALIAS)
+# here you can create your own directory, it can be used either for creating pictures for learning dataset or for test dataset
+# either for pictures with tennis courts or without
         cr_resize_pic.save("/home/kseniia/Pictures/No tennis/"+str(n)+i)
